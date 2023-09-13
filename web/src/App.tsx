8b63047db524126e98e0578d5e610933e6441704
Eddarif Badr <skyzoxcodes@gmail.com>
2023-09-13 13:37:12 +0100

    Tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
       console.log("WebSocket Connected")
     })
     socket.addEventListener("message", (e) => {
-      setMessages((prev) => [...prev!, JSON.parse(e.data)])
+      setMessages((prev) => [...prev, JSON.parse(e.data)])
     })
    
   }, [])
@@ -53,18 +53,19 @@ function App() {
   }
 
   return (
-    <div >
-      <div className='container'>
-        <div>
-          <form onSubmit={SubmitHandler}>
-            <input placeholder='Create a Message' value={message.content} onChange={(e) => setMessage((prev) => ({ ...prev, content: e.target.value }))}></input>
+    <div className='bg-gray-900 text-gray-50'>
+      <div className=''>
+        <div >
+          <form onSubmit={SubmitHandler} className='w-full flex justify-center max-w-7xl mx-auto'>
+            <input placeholder='Create a Message' value={message.content} className='w-[97%] mt-2 mx-2 px-3 py-1 rounded-xl placeholder-gray-100 bg-slate-600' onChange={(e) => setMessage((prev) => ({ ...prev, content: e.target.value }))}></input>
           </form>
         </div>
-        <div>
+        <div className='max-w-7xl mx-auto mt-2'>
           {[...messages].reverse().map((message, index) => {
             return (
-              <div key={index}>
-                <h3>{message.content}</h3>
+              <div key={index} className='px-4 py-3 hover:bg-gray-800'>
+                <time className='text-sm font-mono' dateTime={message.date}>{message.date}</time>
+                <h2 className='font-bold text-xl'>{message.content}</h2>
               </div>
             )
           })}
